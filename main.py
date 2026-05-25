8f60163ad6a9dd7d106546254771113a66ade294
1208nn <74806550+1208nn@users.noreply.github.com>
2026-05-25 23:48:36 +0800

    更新 User-Agent 字段以使用最新的浏览器版本

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ from utils.hash import md5
 from utils.xencode import xencode
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 "
-    " Safari/537.36 Edg/128.0.0.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0"
 }
 auths = []
 
